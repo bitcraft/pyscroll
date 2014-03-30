0197a5a6c76d5e43ee084d3c28b250674bdbe109
Leif Theden <leif.theden@gmail.com>
2014-03-30 16:02:25 -0500

    new setup

--- pyscroll/__init__.py
+++ pyscroll/__init__.py
@@ -1,4 +1,5 @@
-__author__ = 'leif'
-__version__ = "2.04"
-
 from .pyscroll import BufferedRenderer, TiledMapData
+__version__ = '2.06'
+__author__ = 'bitcraft'
+__author_email__ = 'leif.theden@gmail.com'
+__description__ = 'Pygame Scrolling - Python 2.7'
--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ def read(file_name):
 
 setup(name="pyscroll",
       version=pyscroll.__version__,
-      description='Tilemap scrolling utils - python 2.7',
-      author='bitcraft',
+      description=pyscroll.__description__,
+      author=pyscroll.__author__,
+      author_email=pyscroll.__author_email__,
       packages=['pyscroll'],
       install_requires=['pygame'],
       license="LGPLv3",
