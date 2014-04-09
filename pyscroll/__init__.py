36f972bf5c93325b3f932d49d193f45dda379e28
bitcraft <leif.theden@gmail.com>
2014-04-08 23:46:26 -0500

    new control for buffer size, new threading option

--- pyscroll/__init__.py
+++ pyscroll/__init__.py
@@ -1,5 +1,5 @@
 from .pyscroll import BufferedRenderer, ThreadedRenderer, TiledMapData
-__version__ = '2.07'
+__version__ = '2.08'
 __author__ = 'bitcraft'
 __author_email__ = 'leif.theden@gmail.com'
 __description__ = 'Pygame Scrolling - Python 2.7'
